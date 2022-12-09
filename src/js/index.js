f32457a260e055c886a206dd422bd69a9158788b
gerardogarnica <gerardo.garnica@gmail.com>
2022-12-09 09:29:34 -0500

    Fix scrolling when mobile menu is visible.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,27 @@
 'use strict';
 
 const documentReady = () => {
+    const body = document.querySelector(".body");
     const headerNavToggle = document.getElementById("header-nav-toggle");
     const headerNavMenuContainer = document.getElementById("header-nav-menu-container");
     const menuItems = document.querySelectorAll(".header-nav-menu-link-item");
     const sections = document.querySelectorAll(".section");
 
     const toggleMenu = () => {
+        body.classList.toggle("body-no-scroll");
         headerNavToggle.classList.toggle("header-nav-toggle-open");
         headerNavMenuContainer.classList.toggle("header-nav-menu-container-open");
     }
 
     menuItems.forEach((link) =>
         link.addEventListener("click", () => {
+            body.classList.remove("body-no-scroll");
             headerNavToggle.classList.remove("header-nav-toggle-open");
             headerNavMenuContainer.classList.remove("header-nav-menu-container-open");
         })
     );
 
-    window.onscroll = () => {
+    const documentScroll = () => {
         var current = "";
 
         sections.forEach((section) => {
@@ -36,6 +39,7 @@ const documentReady = () => {
         });
     }
 
+    document.addEventListener('scroll', documentScroll);
     headerNavToggle.addEventListener('click', toggleMenu);
 };
 
