a2af989eccd2f372cf6a5741b4ad06826bc1d0ed
gerardogarnica <gerardo.garnica@gmail.com>
2022-12-13 18:00:05 -0500

    Add dark theme and light theme.

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,14 @@
 
 const documentReady = () => {
     const body = document.querySelector(".body");
+    const headerNavLogo = document.getElementById("header-nav-logo");
     const headerNavToggle = document.getElementById("header-nav-toggle");
     const headerNavMenuContainer = document.getElementById("header-nav-menu-container");
+    const headerNavThemeContainer = document.getElementById("header-nav-theme-container");
+    const headerNavThemeIconMoon = document.getElementById("header-nav-theme-icon-moon");
+    const headerNavThemeIconSun = document.getElementById("header-nav-theme-icon-sun");
     const menuItems = document.querySelectorAll(".header-nav-menu-link-item");
     const sections = document.querySelectorAll(".section");
-
-    const toggleMenu = () => {
-        body.classList.toggle("body-no-scroll");
-        headerNavToggle.classList.toggle("header-nav-toggle-open");
-        headerNavMenuContainer.classList.toggle("header-nav-menu-container-open");
-    }
-
-    menuItems.forEach((link) =>
-        link.addEventListener("click", () => {
-            body.classList.remove("body-no-scroll");
-            headerNavToggle.classList.remove("header-nav-toggle-open");
-            headerNavMenuContainer.classList.remove("header-nav-menu-container-open");
-        })
-    );
 
     const documentScroll = () => {
         var current = "";
@@ -39,8 +29,53 @@ const documentReady = () => {
         });
     }
 
+    const toggleMenu = () => {
+        body.classList.toggle("body-no-scroll");
+        headerNavToggle.classList.toggle("header-nav-toggle-open");
+        headerNavMenuContainer.classList.toggle("header-nav-menu-container-open");
+    }
+
+    const toggleTheme = () => {
+        body.classList.toggle("body-light");
+        headerNavThemeIconMoon.classList.toggle("active");
+        headerNavThemeIconSun.classList.toggle("active");
+
+        if (body.classList.contains("body-light")) {
+            localStorage.setItem("darkMode", "false");
+            headerNavLogo.classList.remove("header-nav-logo-dark");
+            headerNavLogo.classList.add("header-nav-logo-light");
+        } else {
+            localStorage.setItem("darkMode", "true");
+            headerNavLogo.classList.add("header-nav-logo-dark");
+            headerNavLogo.classList.remove("header-nav-logo-light");
+        }
+    }
+
+    if (localStorage.getItem("darkMode") === "true" || localStorage.getItem("darkMode") === null) {
+        body.classList.remove("body-light");
+        headerNavLogo.classList.add("header-nav-logo-dark");
+        headerNavLogo.classList.remove("header-nav-logo-light");
+        headerNavThemeIconMoon.classList.remove("active");
+        headerNavThemeIconSun.classList.add("active");
+    } else {
+        body.classList.add("body-light");
+        headerNavLogo.classList.remove("header-nav-logo-dark");
+        headerNavLogo.classList.add("header-nav-logo-light");
+        headerNavThemeIconMoon.classList.add("active");
+        headerNavThemeIconSun.classList.remove("active");
+    }
+
+    menuItems.forEach((link) =>
+        link.addEventListener("click", () => {
+            body.classList.remove("body-no-scroll");
+            headerNavToggle.classList.remove("header-nav-toggle-open");
+            headerNavMenuContainer.classList.remove("header-nav-menu-container-open");
+        })
+    );
+
     document.addEventListener('scroll', documentScroll);
     headerNavToggle.addEventListener('click', toggleMenu);
+    headerNavThemeContainer.addEventListener('click', toggleTheme);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
