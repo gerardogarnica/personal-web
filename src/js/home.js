997bec6f900f286fc046812a9c0855d00686fd98
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-01 12:02:45 -0500

    Change home and contact sections

--- src/js/home.js
+++ src/js/home.js
@@ -4,19 +4,19 @@ const setHomeInfo = ({ home }) => {
   const homeInfoBegin = document.querySelector('.home-info-begin')
   const homeInfoName = document.querySelector('.home-info-name')
   const homeInfoTitle = document.querySelector('.home-info-title')
-  const homeInfoMainDescription = document.querySelector('.home-info-main-description')
-  const homeInfoSecondaryDescription = document.querySelector('.home-info-secondary-description')
-  const homeCurriculumLink = document.getElementById('home-curriculum-link')
-  const homeCurriculumText = document.querySelector('.home-curriculum-text')
+  //const homeInfoMainDescription = document.querySelector('.home-info-main-description')
+  //const homeInfoSecondaryDescription = document.querySelector('.home-info-secondary-description')
+  //const homeCurriculumLink = document.getElementById('home-curriculum-link')
+  //const homeCurriculumText = document.querySelector('.home-curriculum-text')
 
   console.log(home)
   homeInfoBegin.innerHTML = 'Hola! Soy'
   homeInfoName.innerHTML = name
   homeInfoTitle.innerHTML = title
-  homeInfoMainDescription.innerHTML = mainDescription
-  homeInfoSecondaryDescription.innerHTML = secondaryDescription
-  homeCurriculumLink.href = cvUri
-  homeCurriculumText.innerHTML = 'Descargar CV'
+  //homeInfoMainDescription.innerHTML = mainDescription
+  //homeInfoSecondaryDescription.innerHTML = secondaryDescription
+  //homeCurriculumLink.href = cvUri
+  //homeCurriculumText.innerHTML = 'Descargar CV'
 }
 
 export default setHomeInfo
