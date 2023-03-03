051b57b8991c9e21b53146852430c80f76d87eba
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-03 08:36:30 -0500

    Change home layout.

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,12 @@
 const setHomeInfo = ({ home }) => {
-  const { id, name, title } = home
+  const { id, intro, name, title } = home
 
   const homeInfoBegin = document.querySelector('.home-info-begin')
   const homeInfoName = document.querySelector('.home-info-name')
   const homeInfoTitle = document.querySelector('.home-info-title')
 
   console.log(home)
-  homeInfoBegin.innerHTML = 'Hola! Soy'
+  homeInfoBegin.innerHTML = intro
   homeInfoName.innerHTML = name
   homeInfoTitle.innerHTML = title
 }
