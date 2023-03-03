0924ef404c35d2c47dd48a71a0abc5ee1fb600de
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-02 22:00:17 -0500

    Add about section.

--- src/js/about.js
+++ src/js/about.js
@@ -1,15 +1,17 @@
 const setAboutInfo = ({ about }) => {
-  const { mainDescription, secondaryDescription, cvUri } = about
+  const { mainDescription, secondaryDescription, cvUri, skillsTitle } = about
 
   const aboutInfoMainDescription = document.querySelector('.about-info-main-description')
   const aboutInfoSecondaryDescription = document.querySelector('.about-info-secondary-description')
-  //const aboutCurriculumLink = document.getElementById('about-curriculum-link')
-  //const aboutCurriculumText = document.querySelector('.about-curriculum-text')
+  const aboutCurriculumLink = document.getElementById('about-curriculum-link')
+  const aboutCurriculumText = document.querySelector('.about-curriculum-text')
+  const aboutSkillsTitle = document.querySelector('.about-info-skills-title')
 
   aboutInfoMainDescription.innerHTML = mainDescription
   aboutInfoSecondaryDescription.innerHTML = secondaryDescription
-  //homeCurriculumLink.href = cvUri
-  //homeCurriculumText.innerHTML = 'Descargar CV'
+  aboutCurriculumLink.href = cvUri
+  aboutCurriculumText.innerHTML = 'Descargar CV'
+  aboutSkillsTitle.innerHTML = skillsTitle
 }
 
 export default setAboutInfo
