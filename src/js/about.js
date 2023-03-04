de766111e96a3fcf1fa046a07c931bba157311e0
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-03 21:36:55 -0500

    Change skills section.

--- src/js/about.js
+++ src/js/about.js
@@ -1,17 +1,30 @@
 const setAboutInfo = ({ about }) => {
-  const { mainDescription, secondaryDescription, cvUri, skillsTitle } = about
+  const { mainDescription, secondaryDescription, cvUri, skillsTitle, skills } = about
 
   const aboutInfoMainDescription = document.querySelector('.about-info-main-description')
   const aboutInfoSecondaryDescription = document.querySelector('.about-info-secondary-description')
   const aboutCurriculumLink = document.getElementById('about-curriculum-link')
   const aboutCurriculumText = document.querySelector('.about-curriculum-text')
   const aboutSkillsTitle = document.querySelector('.about-info-skills-title')
+  const aboutSkills = document.getElementById('about-skills')
 
   aboutInfoMainDescription.innerHTML = mainDescription
   aboutInfoSecondaryDescription.innerHTML = secondaryDescription
   aboutCurriculumLink.href = cvUri
   aboutCurriculumText.innerHTML = 'Descargar CV'
   aboutSkillsTitle.innerHTML = skillsTitle
+
+  aboutSkills.innerHTML = getSkillsHtml(skills)
+}
+
+const getSkillsHtml = (skills) => {
+  let skillsHtml = ''
+
+  for (let i = 0; i < skills.length; i++) {
+    skillsHtml += `<img src="./assets/skills/${skills[i].imageFile}" loading="lazy" alt="${skills[i].title}" title="${skills[i].title}" />`
+  }
+
+  return skillsHtml
 }
 
 export default setAboutInfo
